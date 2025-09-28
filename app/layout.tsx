c7203b511f50a9e2c074c610d447f9f35fcff9d5
EntchenEric <eseidel2004@gmail.com>
2025-09-28 13:19:06 +0200

    initiate project with button and heroSection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { EntchenProvider } from "@/components/EntchenProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <EntchenProvider theme={{
+          primaryColor: "yellow",
+          secondaryColor: "orange",
+          textColor: "black",
+          textSecondaryColor: 'black'
+        }}>
+          {children}
+        </EntchenProvider>
       </body>
     </html>
   );
